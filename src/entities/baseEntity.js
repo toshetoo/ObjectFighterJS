80ba208d0353262697a455e134c95827e0b7a0a5
Todor Ivanov <ivanov.todor1@gmail.com>
2018-01-12 14:42:10 +0200

    Add an option for the fighters to miss an attack

--- src/entities/baseEntity.js
+++ src/entities/baseEntity.js
@@ -12,7 +12,7 @@ export class BaseEntity {
 
     hit() {
         const num = Helpers.getRandomNumber(0, 101);
-        if(num < 80) {
+        if (num < 80) {
             return this.normalAttack();
         } else {
             return this.specialAttack();
@@ -20,16 +20,30 @@ export class BaseEntity {
     }
 
     normalAttack() {
-        const dmg = Math.ceil(this.strength * 0.4 + Helpers.getRandomNumber(0,40)); // some formula for hit;
-        const msg = `<div class="attack-div normal-attack text-center">${this.name} performed a normal attack causing ${dmg} damage. </div>`;
-        Logger.log(msg);
-        return dmg;
+        const shouldHit = Helpers.getRandomNumber(0, 101);
+        if (shouldHit < 10) {
+            const msg = `<div class="attack-div missed-attack text-center">${this.name} tried to perform a normal attack but missed. No damage was inflicted</div>`;
+            Logger.log(msg);
+            return 0;
+        } else {
+            const dmg = Math.ceil(this.strength * 0.4 + Helpers.getRandomNumber(0, 40)); // some formula for hit;
+            const msg = `<div class="attack-div normal-attack text-center">${this.name} performed a normal attack causing ${dmg} damage. </div>`;
+            Logger.log(msg);
+            return dmg;
+        }
     }
 
     specialAttack() {
-        const dmg = Math.ceil(this.strength * 0.8 + Helpers.getRandomNumber(20,80)); // some formula for hit;
-        const msg = `<div class="attack-div special-attack text-center">${this.name} performed a special attack causing ${dmg} damage. </div>`;
-        Logger.log(msg);
-        return dmg;
+        const shouldHit = Helpers.getRandomNumber(0, 101);
+        if (shouldHit < 10) {
+            const msg = `<div class="attack-div missed-attack text-center">${this.name} tried to perform a special attack but missed. No damage was inflicted</div>`;
+            Logger.log(msg);
+            return 0;
+        } else {
+            const dmg = Math.ceil(this.strength * 0.8 + Helpers.getRandomNumber(20, 80)); // some formula for hit;
+            const msg = `<div class="attack-div special-attack text-center">${this.name} performed a special attack causing ${dmg} damage. </div>`;
+            Logger.log(msg);
+            return dmg;
+        }
     }
 }
